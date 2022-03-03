b36089b5fb83e152666dfbd0202a4c4788d5c3e8
trsimanto <simanto14feb@gmail.com>
2022-03-03 04:02:52 -0500

    commit with textfield for sent msg

--- SwiftUI_Chat_App/ContentView.swift
+++ SwiftUI_Chat_App/ContentView.swift
@@ -12,17 +12,21 @@ struct ContentView: View {
     var messageArray = ["Hello you","How are you doing?","I've been buliding SwiftUI applications from scratch and it's so much fun!"]
     var body: some View {
         VStack {
-             TitleRow()
-            ScrollView{
-                ForEach(messageArray, id: \.self ){ text in
-                    MassageBubble(message: Message(id: "12345", text: text, received: true, timestamp: Date()))
+            VStack {
+                 TitleRow()
+                ScrollView{
+                    ForEach(messageArray, id: \.self ){ text in
+                        MassageBubble(message: Message(id: "12345", text: text, received: true, timestamp: Date()))
 
-                }
-            }.padding(.top , 10)
-            .background(.white)
-                
-        }.frame(maxWidth: .infinity)
-        .background(Color("Peach"))
+                    }
+                }.padding(.top , 10)
+                .background(.white)
+                    
+            }.frame(maxWidth: .infinity)
+                .background(Color("Peach"))
+            MessageField()
+        }
+       
     }
 }
 
